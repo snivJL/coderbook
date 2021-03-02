56ae97a4a04d1cff12e8caa1fb95b53bbc9d1f43
Loi Van Tran <datloiboi@gmail.com>
2021-03-02 15:00:52 +0700

    Update create User Modal

--- client/src/components/Composer/Composer.js
+++ client/src/components/Composer/Composer.js
@@ -6,9 +6,9 @@ import "./style.css";
 
 const ComposerButton = ({ title, icon }) => {
   return (
-    <Button className="d-flex justify-content-center align-items-center bg-white text-dark border-0 rounded">
+    <Button className="d-flex justify-content-center align-items-center bg-light bg-white text-dark border-0 rounded">
       {" "}
-      <FontAwesomeIcon icon={icon} className="mr-2" size="lg"/>
+      <FontAwesomeIcon icon={icon} className="mr-2" size="lg" />
       {title}
     </Button>
   );
@@ -17,14 +17,20 @@ const ComposerButton = ({ title, icon }) => {
 export default function Composer() {
   return (
     <Card className="mb-3 w-100">
-      <Card.Body>
+      <Card.Body className="px-3 pt-3">
         {" "}
+        {/* STEP 2 */}
         <Form>
           <Form.Group>
-            <Form.Control type="text" placeholder="What's on your mind?" />
+            <Form.Control
+              type="text"
+              placeholder="What's on your mind?"
+              className="post-text"
+            />
           </Form.Group>
         </Form>
       </Card.Body>
+      <hr className="mt-0" />
       <ButtonGroup size="lg" className="m-2">
         <ComposerButton title="Live Video" icon="video" />
         <ComposerButton title="Photo Video" icon="photo-video" />
