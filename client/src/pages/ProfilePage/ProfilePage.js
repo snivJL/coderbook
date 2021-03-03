67dde71ef8ad2b8ca19b107ce21a1a4c7edec539
Loi Van Tran <datloiboi@gmail.com>
2021-03-03 15:56:26 +0700

    Fix avatar on homepage

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -9,18 +9,20 @@ import Composer from "../../components/Composer/Composer";
 export default function ProfilePage() {
   return (
     <div>
-      <Row className="centered hero">
-        <Container className="d-flex align-items-center justify-content-center mb-5 cover-container">
+      <Row className="centered hero mh-75">
+        <Container className="d-flex flex-column align-items-center justify-content-center">
           <img
             alt="lighthouse"
             className="img-fluid rounded-md"
             src="https://images.unsplash.com/photo-1507725914440-e1e434774828?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&w=2389&q=100"
           />
-          <img
-            alt="profile"
-            className="rounded-circle cover-profile-photo"
-            src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
-          />
+          <div className="centered position-relative">
+            <img
+              alt="profile"
+              className="rounded-circle position-absolute cover-profile-photo"
+              src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
+            />
+          </div>
         </Container>
         <hr className="w-75" />
       </Row>
