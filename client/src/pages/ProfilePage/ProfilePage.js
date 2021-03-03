7ce4ac894a5b9075a5d3b5287b22501d36df2bfc
Loi Van Tran <datloiboi@gmail.com>
2021-03-03 16:14:03 +0700

    Delete unneeded classes

--- client/src/pages/ProfilePage/ProfilePage.js
+++ client/src/pages/ProfilePage/ProfilePage.js
@@ -9,24 +9,24 @@ import Composer from "../../components/Composer/Composer";
 export default function ProfilePage() {
   return (
     <div>
-      <Row className="centered hero mh-75">
-        <Container className="d-flex flex-column align-items-center justify-content-center">
+      <Row className="centered hero">
+        <Container className="centered flex-column">
           <img
             alt="lighthouse"
-            className="img-fluid rounded-md"
+            className="position-relative img-fluid rounded-md"
             src="https://images.unsplash.com/photo-1507725914440-e1e434774828?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&w=2389&q=100"
           />
           <div className="centered position-relative">
             <img
               alt="profile"
-              className="rounded-circle position-absolute cover-profile-photo"
+              className="position-absolute rounded-circle cover-profile-photo"
               src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg"
             />
           </div>
         </Container>
         <hr className="w-75" />
       </Row>
-      <Row className="bg-white rounded profile-nav">
+      <Row className="rounded profile-nav bg-white">
         <Container className="centered">
           <Container>
             <Nav
